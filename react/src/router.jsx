5852a7c6f5c0497acd1e6f9b9669e226d5d764c0
NikKowPHP <nik.kow@outlook.com>
2023-05-02 08:38:45 +0200

    modify routes: destribute guest and default

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,31 +1,42 @@
-import {createBrowserRouter}	from "react-router-dom"	
+import { createBrowserRouter } from "react-router-dom";
 import Login from "./views/Login";
 import Signup from "./views/Signup";
 import Users from "./views/Users";
 import NotFound from "./views/NotFound";
+import Dashboard from "./views/Dashboard.jsx";
+import DefaultLayout from "./components/DefaultLayout";
+import GuestLayout from "./components/GuestLayout";
 
-const router = createBrowserRouter( [
-	{
-		path: '/',
-		element: <Login />
-	},
-	{
-		path: '/login',
-		element: <Login />
-	},
-	{
-		path: '/signup',
-		element: <Signup/>
-	},
-	{
-		path: '/login',
-		element: <Users/>
-	},
-	{
-		path: '*',
-		element: <NotFound/>
-	},
-
-])
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: "/dashboard",
+                element: <Dashboard/>,
+								
+            },
+        ],
+    },
+    {
+        path: "/",
+        element: <GuestLayout />,
+        children: [
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/signup",
+                element: <Signup />,
+            },
+        ],
+    },
+    {
+        path: "*",
+        element: <NotFound />,
+    },
+]);
 
 export default router;
