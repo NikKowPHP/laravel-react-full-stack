03d28da0a7ced7ff98322a73bb9691a7ab9f5e7e
NikKowPHP <nik.kow@outlook.com>
2023-05-02 08:19:12 +0200

    create basic routes in router

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,6 +1,30 @@
 import {createBrowserRouter}	from "react-router-dom"	
+import Login from "./views/Login";
+import Signup from "./views/Signup";
+import Users from "./views/Users";
+import NotFound from "./views/NotFound";
 
 const router = createBrowserRouter( [
+	{
+		path: '/',
+		element: <Login />
+	},
+	{
+		path: '/login',
+		element: <Login />
+	},
+	{
+		path: '/signup',
+		element: <Signup/>
+	},
+	{
+		path: '/login',
+		element: <Users/>
+	},
+	{
+		path: '*',
+		element: <NotFound/>
+	},
 
 ])
 
