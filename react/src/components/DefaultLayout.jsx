adbe5e229aad25afe7c692fee928916c23fbefc4
NikKowPHP <nik.kow@outlook.com>
2023-05-03 17:36:28 +0200

    create default layout

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 import { useStateContext } from '../contexts/ContextProvider.jsx'
 import { Navigate } from 'react-router-dom';
 
@@ -9,9 +9,25 @@ export default function DefaultLayout() {
 		return <Navigate to= "/login" />
 	}
 	return (
-		<div>
-			default
-			<Outlet />
+		<div id="defaultLayout">
+			<aside>
+				<Link to="/dashboard">Dashboard</Link>
+				<Link to="/users">users</Link>
+			</aside>
+			<div className="content">
+				<header>
+					<div>
+						Header
+					</div>
+					<div>
+						User info
+					</div>
+				</header>
+				<main>
+					<Outlet />
+					
+				</main>
+			</div>
 		</div>
 	)
 }
