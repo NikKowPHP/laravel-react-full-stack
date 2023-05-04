804d3363b4932f272d48daad332dd177ba0a8d1a
NikKowPHP <nik.kow@outlook.com>
2023-05-04 07:56:43 +0200

    create login and logout functionallity

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,12 +7,10 @@ use Illuminate\Http\Request;
 use App\Http\Requests\LoginRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SignupRequest;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(LoginRequest $loginRequest)
-    {
-    }
     public function signup(SignupRequest $signupRequest)
     {
         $data = $signupRequest->validated();
@@ -26,8 +24,23 @@ class AuthController extends Controller
         return response(compact('user', 'token'));
 
     }
+    public function login(LoginRequest $loginRequest)
+    {
+        $credentials = $loginRequest->validated();
+        if(Auth::attempt($credentials)) {
+            return response([
+                'message' => 'Provided email address or password is incorrect'
+            ]);
+        }
+        $user = Auth::user();
+       $token =  $user->createToken('main')->plainTextToken;
+       return response(compact('user', 'token'));
+    }
     public function logout(Request $request)
     {
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+        return response('', 204);
 
     }
 }
