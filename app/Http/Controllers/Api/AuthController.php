7641f1ddcc3fded53f30a6536bf0b1890bf32c32
NikKowPHP <nik.kow@outlook.com>
2023-05-04 07:37:39 +0200

    create authController , create signup function

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SignupRequest;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $loginRequest)
+    {
+    }
+    public function signup(SignupRequest $signupRequest)
+    {
+        $data = $signupRequest->validated();
+       $user =  User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response(compact('user', 'token'));
+
+    }
+    public function logout(Request $request)
+    {
+
+    }
+}
