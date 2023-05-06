6f9223ef1a6cc2f5a033f11f53b2fecdc8c2b5b6
NikKowPHP <nik.kow@outlook.com>
2023-05-06 08:27:40 +0200

    create a route for logout

--- routes/api.php
+++ routes/api.php
@@ -15,13 +15,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->group(function() {
+
+Route::get('/user', function (Request $request) {
     return $request->user();
+});
+    //  logout user
+    Route::post('/logout', [AuthController::class, 'logout' ]);
 });
 // signup user
 Route::post('/signup', [AuthController::class, 'signup' ]);
 Route::get('/signup', [AuthController::class, 'logout' ]);
 // login user
 Route::post('/login', [AuthController::class, 'login' ]);
-// logout user
-Route::post('/logout', [AuthController::class, 'logout' ]);
+Route::get('/login', [AuthController::class, 'login']);
