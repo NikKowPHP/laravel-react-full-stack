f41a6b3c4aabbdcb0c6d393c08f6610ccf06ade4
NikKowPHP <nik.kow@outlook.com>
2023-05-06 09:32:31 +0200

    create link to add users, get users with axios

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
-
+import axiosClient from '../axios-client';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 export default function Users() {
+
+	const [users, setUsers] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	useEffect(()=> {
+		getUsers();
+	}, [])
+
+	const getUsers = () => {
+		setLoading(true)
+		axiosClient.get('/users')
+		.then(({data}) => {
+			setLoading(false)
+			console.log(data);
+		})
+		.catch(() => {
+			setLoading(false)
+		})
+	}
 	return (
-		<div>Users</div>
+		<div>
+			<div>
+				<h1>Users</h1>
+				<Link to="/users/new">Add new</Link>
+			</div>
+
+		</div>
 	)
 }
